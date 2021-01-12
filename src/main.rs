4e4576eb9e25faf57325e9395e27c4e6d99ad47c
illumination-k <illumination.k.27@gmail.com>
2021-01-13 01:15:13 +0900

    add raw_markdown for test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+extern crate pulldown_cmark;
+extern crate anyhow;
+extern crate mecab;
+
+use anyhow::Result;
+use pulldown_cmark::{Parser, Options};
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
